73d5de963b4f49a9631083b7b9d2ee41b9af2d1f
hugo <hugo.arenas@usach.cl>
2021-08-02 19:05:17 -0400

    avance 01

--- Lab3_AD.R
+++ Lab3_AD.R
@@ -78,55 +78,116 @@ summary(tabla)
 
 #Se realizan gráficos de cajas de cada variable con respecto a las clases.
 #Se realiza el gráfico de cajas por clase respecto a la variables "age".
-boxplot.age =  ggboxplot(data = tabla, x = "class", y = "age", color = "class", add = "jitter") + border() 
+boxplot.age = ggboxplot(data = tabla, 
+                        x = "class", 
+                        y = "age", 
+                        color = "class", 
+                        add = "jitter") + border() 
 ydens = axis_canvas(boxplot.age, axis = "y", coord_flip = TRUE) + geom_density(data = tabla, aes(x = age, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
 boxplot.age = insert_yaxis_grob(boxplot.age, ydens, grid::unit(.2, "null"), position = "right")
-ggdraw(boxplot.age)
+print(ggdraw(boxplot.age))
 
 #Se realiza el gráfico de cajas por clase respecto a la variables "menopause".
-boxplot.menopause =  ggboxplot(data = tabla, x = "class", y = "menopause", color = "class", add = "jitter") + border() 
+boxplot.menopause = ggboxplot(data = tabla, 
+                              x = "class", 
+                              y = "menopause", 
+                              color = "class", 
+                              add = "jitter") + border() 
 ydens = axis_canvas(boxplot.menopause, axis = "y", coord_flip = TRUE) + geom_density(data = tabla, aes(x = menopause, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
 boxplot.menopause = insert_yaxis_grob(boxplot.menopause, ydens, grid::unit(.2, "null"), position = "right")
-ggdraw(boxplot.menopause)
+print(ggdraw(boxplot.menopause))
 
 #Se realiza el gráfico de cajas por clase respecto a la variables "tumor.size".
-boxplot.tumor.size =  ggboxplot(data = tabla, x = "class", y = "tumor.size", color = "class", add = "jitter") + border() 
+boxplot.tumor.size = ggboxplot(data = tabla, 
+                               x = "class", 
+                               y = "tumor.size", 
+                               color = "class", 
+                               add = "jitter") + border() 
 ydens = axis_canvas(boxplot.tumor.size, axis = "y", coord_flip = TRUE) + geom_density(data = tabla, aes(x = tumor.size, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
 boxplot.tumor.size = insert_yaxis_grob(boxplot.tumor.size, ydens, grid::unit(.2, "null"), position = "right")
-ggdraw(boxplot.tumor.size)
+print(ggdraw(boxplot.tumor.size))
 
 #Se realiza el gráfico de cajas por clase respecto a la variables "inv.nodes".
-boxplot.inv.nodes =  ggboxplot(data = tabla, x = "class", y = "inv.nodes", color = "class", add = "jitter") + border() 
+boxplot.inv.nodes =  ggboxplot(data = tabla, 
+                               x = "class", 
+                               y = "inv.nodes", 
+                               color = "class", 
+                               add = "jitter") + border() 
 ydens = axis_canvas(boxplot.inv.nodes, axis = "y", coord_flip = TRUE) + geom_density(data = tabla, aes(x = inv.nodes, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
 boxplot.inv.nodes = insert_yaxis_grob(boxplot.inv.nodes, ydens, grid::unit(.2, "null"), position = "right")
-ggdraw(boxplot.inv.nodes)
+print(ggdraw(boxplot.inv.nodes))
 
 #Se realiza el gráfico de cajas por clase respecto a la variables "node.caps".
-boxplot.node.caps =  ggboxplot(data = tabla, x = "class", y = "node.caps", color = "class", add = "jitter") + border() 
+boxplot.node.caps = ggboxplot(data = tabla, 
+                              x = "class", 
+                              y = "node.caps", 
+                              color = "class", 
+                              add = "jitter") + border() 
 ydens = axis_canvas(boxplot.node.caps, axis = "y", coord_flip = TRUE) + geom_density(data = tabla, aes(x = node.caps, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
 boxplot.node.caps = insert_yaxis_grob(boxplot.node.caps, ydens, grid::unit(.2, "null"), position = "right")
-ggdraw(boxplot.node.caps)
+print(ggdraw(boxplot.node.caps))
 
 #Se realiza el gráfico de cajas por clase respecto a la variables "deg.malig".
-boxplot.deg.malig =  ggboxplot(data = tabla, x = "class", y = "deg.malig", color = "class", add = "jitter") + border() 
+boxplot.deg.malig =  ggboxplot(data = tabla, 
+                               x = "class", 
+                               y = "deg.malig", 
+                               color = "class", 
+                               add = "jitter") + border() 
 ydens = axis_canvas(boxplot.deg.malig, axis = "y", coord_flip = TRUE) + geom_density(data = tabla, aes(x = deg.malig, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
 boxplot.deg.malig = insert_yaxis_grob(boxplot.deg.malig, ydens, grid::unit(.2, "null"), position = "right")
-ggdraw(boxplot.deg.malig)
+print(ggdraw(boxplot.deg.malig))
 
 #Se realiza el gráfico de cajas por clase respecto a la variables "breast".
-boxplot.breast =  ggboxplot(data = tabla, x = "class", y = "breast", color = "class", add = "jitter") + border() 
+boxplot.breast = ggboxplot(data = tabla, 
+                           x = "class", 
+                           y = "breast", 
+                           color = "class", 
+                           add = "jitter") + border() 
 ydens = axis_canvas(boxplot.breast, axis = "y", coord_flip = TRUE) + geom_density(data = tabla, aes(x = breast, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
 boxplot.breast = insert_yaxis_grob(boxplot.breast, ydens, grid::unit(.2, "null"), position = "right")
-ggdraw(boxplot.breast)
+print(ggdraw(boxplot.breast))
 
 #Se realiza el gráfico de cajas por clase respecto a la variables "breast.quad".
-boxplot.breast.quad =  ggboxplot(data = tabla, x = "class", y = "breast.quad", color = "class", add = "jitter") + border() 
+boxplot.breast.quad = ggboxplot(data = tabla, 
+                                x = "class", 
+                                y = "breast.quad", 
+                                color = "class", 
+                                add = "jitter") + border() 
 ydens = axis_canvas(boxplot.breast.quad, axis = "y", coord_flip = TRUE) + geom_density(data = tabla, aes(x = breast.quad, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
 boxplot.breast.quad = insert_yaxis_grob(boxplot.breast.quad, ydens, grid::unit(.2, "null"), position = "right")
-ggdraw(boxplot.breast.quad)
+print(ggdraw(boxplot.breast.quad))
 
 #Se realiza el gráfico de cajas por clase respecto a la variables "irradiat".
-boxplot.irradiat =  ggboxplot(data = tabla, x = "class", y = "irradiat", color = "class", add = "jitter") + border() 
+boxplot.irradiat = ggboxplot(data = tabla, 
+                             x = "class", 
+                             y = "irradiat", 
+                             color = "class", 
+                             add = "jitter") + border() 
 ydens = axis_canvas(boxplot.irradiat, axis = "y", coord_flip = TRUE) + geom_density(data = tabla, aes(x = irradiat, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
 boxplot.irradiat = insert_yaxis_grob(boxplot.irradiat, ydens, grid::unit(.2, "null"), position = "right")
-ggdraw(boxplot.irradiat)
+print(ggdraw(boxplot.irradiat))
+
+#Se crean las reglas para las variables "age", "tumor.size" e "inv.nodes".
+tabla.reglas = tabla
+age = c(-Inf, 2, 4, Inf)
+age.names = c("adulto joven", "adulto", "adulto mayor")
+
+tumor.size = c(-Inf, 4, 7, Inf)
+tumor.size.names = c("pequeño", "mediano", "grande")
+
+inv.nodes = c(-Inf, 2, 5, Inf)
+inv.nodes.names = c("bajo", "medio", "alto")
+
+#Se cambian variables por las reglas asociadas.
+tabla.reglas$age = cut(tabla.reglas$age, breaks = age, labels = age.names)
+tabla.reglas$tumor.size = cut(tabla.reglas$tumor.size, breaks = tumor.size, labels = tumor.size.names)
+tabla.reglas$inv.nodes = cut(tabla.reglas$inv.nodes, breaks = inv.nodes, labels = inv.nodes.names)
+
+
+reglas = apriori(
+  data = tabla.reglas, 
+  parameter=list(support = 0.2, minlen = 2, maxlen = 6, target="rules"),
+  appearance=list(rhs = c("class=no-recurrence-events", "class=recurrence-events"))
+)
+
+inspect(sort(x = reglas, decreasing = TRUE, by = "confidence"))
